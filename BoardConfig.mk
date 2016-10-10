3374d06f3ee98baf27171c2a7d3de7b21d83f680
Kshitij Gupta <kshitijgm@gmail.com>
2016-10-10 14:51:37 +0000

    taoshan: kernel-headers: Use separate directory

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,7 @@ DEVICE_PATH := device/sony/taoshan
 
 # Board device headers
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
+PRODUCT_VENDOR_KERNEL_HEADERS :=  $(DEVICE_PATH)/kernel-headers
 
 # Board device elements
 include $(DEVICE_PATH)/PlatformConfig.mk
