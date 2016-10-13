b39c389be005711fd6a2bca5b1af3c97f3f254aa
Kshitij Gupta <kshitijgm@gmail.com>
2016-10-13 12:09:34 +0000

    taoshan: Try to enable Radio

--- product/qcom-fm.mk
+++ product/qcom-fm.mk
@@ -1,5 +1,7 @@
 # FM packages
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/libqcomfm_jni.so:system/lib/libqcomfm_jni.so
 PRODUCT_PACKAGES += \
     FM2 \
+    FMRecord \
     libqcomfm_jni \
     qcom.fmradio
