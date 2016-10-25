30be2df6d1a6d00df87eb56949f858cf28b251d2
Kshitij Gupta <kshitijgm@gmail.com>
2016-10-25 10:58:24 +0000

    taoshan: Build FMRadio

--- product/qcom-fm.mk
+++ product/qcom-fm.mk
@@ -1,7 +1,4 @@
 # FM packages
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/libqcomfm_jni.so:system/lib/libqcomfm_jni.so
 PRODUCT_PACKAGES += \
-    FM2 \
-    FMRecord \
-    libqcomfm_jni \
-    qcom.fmradio
+    FMRadio \
+    libfmjni
